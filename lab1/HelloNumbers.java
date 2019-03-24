9e800d39e3b2400cb7511c1fc9c74ca092b50600
KIMJUNGMIN <testprogramms9104@gmail.com>
2019-03-24 17:56:40 +0900

    completed first part of lab1

---
+++ lab1/HelloNumbers.java
@@ -0,0 +1,12 @@
+public class HelloNumbers
+{
+  public static void main(String[] args)
+  {
+    int result = 0;
+    for(int i=0; i < 10; i++)
+    {
+      System.out.print(result + " ")
+      result += i
+    }
+  }
+}
---
+++ lab1/HelloWorld.java
@@ -0,0 +1,7 @@
+public class HelloWorld
+{
+  public static void method(String[] args)
+  {
+    System.out.println("Hello world");
+  }
+}
